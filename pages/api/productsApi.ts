ef704923d0e10434f2c25995a671505e9a92f8b7
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-10-06 22:34:35 -0300

    :bug: Adjust when click in product to open right image

--- pages/api/productsApi.ts
+++ pages/api/productsApi.ts
@@ -2,23 +2,23 @@ import { Product } from "../../src/models/product";
 
 const products = [
   new Product('Produto XYZ', 'R$ 60,00', '/mug_stropper.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/lego_darth.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/master_yoda.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/stormtropper.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/the_child.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/kylo_ren.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/xbox_control.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/ps5.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/nintendo.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/switch_control.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/xbox.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/game_boy.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/atari.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/shirt_snes.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/sonic.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/retro.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/ar.png', '111111'),
-  new Product('Produto XYZ', 'R$ 60,00', '/pikachu.png', '111111')
+  new Product('Produto XYZ', 'R$ 60,00', '/lego_darth.png', '111112'),
+  new Product('Produto XYZ', 'R$ 60,00', '/master_yoda.png', '111113'),
+  new Product('Produto XYZ', 'R$ 60,00', '/stormtropper.png', '111114'),
+  new Product('Produto XYZ', 'R$ 60,00', '/the_child.png', '111115'),
+  new Product('Produto XYZ', 'R$ 60,00', '/kylo_ren.png', '111116'),
+  new Product('Produto XYZ', 'R$ 60,00', '/xbox_control.png', '111117'),
+  new Product('Produto XYZ', 'R$ 60,00', '/ps5.png', '111118'),
+  new Product('Produto XYZ', 'R$ 60,00', '/nintendo.png', '111119'),
+  new Product('Produto XYZ', 'R$ 60,00', '/switch_control.png', '111120'),
+  new Product('Produto XYZ', 'R$ 60,00', '/xbox.png', '111121'),
+  new Product('Produto XYZ', 'R$ 60,00', '/game_boy.png', '111122'),
+  new Product('Produto XYZ', 'R$ 60,00', '/atari.png', '111123'),
+  new Product('Produto XYZ', 'R$ 60,00', '/shirt_snes.png', '111124'),
+  new Product('Produto XYZ', 'R$ 60,00', '/sonic.png', '111125'),
+  new Product('Produto XYZ', 'R$ 60,00', '/retro.png', '111126'),
+  new Product('Produto XYZ', 'R$ 60,00', '/ar.png', '111127'),
+  new Product('Produto XYZ', 'R$ 60,00', '/pikachu.png', '111128')
 ];
 
 export function getAllProducts() {  
