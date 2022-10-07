c14dcffcac9ab134278c746b16c9e801d1ef955e
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-10-06 22:43:19 -0300

    :sparkles: Adding title in tab product details

--- src/screens/ProductDetailsScreen/index.tsx
+++ src/screens/ProductDetailsScreen/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Column } from "../../components/Grid/Column";
 import AppBar from "../../components/Surfaces/AppBar";
 import { Product } from "../../models/product";
@@ -9,13 +10,20 @@ import ProductsList from "../../patterns/ProductsList";
 const ProductDetailsScreen = (product: Product, productListSimilar: Product[]) => {
 
   return (
-    <Column>
-      <AppBar />
-      <ProductDetails product={product} />      
-      <ProductsList productsList={productListSimilar} title={'Produtos similares'} showSeeProduct={true}/>
-      <FooterContact />
-      <FooterDev devName="Desenvolvido por Lucas Freitas" year='2022' />
-    </Column>
+    <>
+      <Head>
+        <title>{product.name}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Column>
+        <AppBar />
+        <ProductDetails product={product} />
+        <ProductsList productsList={productListSimilar} title={'Produtos similares'} showSeeProduct={true} />
+        <FooterContact />
+        <FooterDev devName="Desenvolvido por Lucas Freitas" year='2022' />
+      </Column>
+    </>
+
 
   )
 }
